ec24d72ed375e3b7097d02406f4f7922c58b62d3
Pavlo Bezkorovainyi <86742948+pavlo-bezkorovainyi@users.noreply.github.com>
2023-10-04 16:18:37 -0700

    Add files via upload

---
+++ MonobankCopy/MonobankCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonobankCopyApp.swift
+//  MonobankCopy
+//
+//  Created by Павел Бескоровайный on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MonobankCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
