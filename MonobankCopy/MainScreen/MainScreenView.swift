113e012fa9cce7d73a3ffb8ba8aaeaed09ba62c1
Pavlo Bezkorovainyi <beskor1993@icloud.com>
2023-10-04 16:24:18 -0700

    MainScreen added

---
+++ MonobankCopy/MainScreen/MainScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreenView.swift
+//  MonobankCopy
+//
+//  Created by Павел Бескоровайный on 04.10.2023.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct MainScreenView_Previews: PreviewProvider {
+  static var previews: some View {
+    MainScreenView()
+  }
+}
